46b074ea084b3985739f16706131f75f6a0520f6
keshew <artyom.korotckov2017@yandex.ru>
2024-11-28 18:46:32 +0500

    Initial Commit

---
+++ Bananza1.1/Bananza1_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Bananza1_1App.swift
+//  Bananza1.1
+//
+//  Created by Артём Коротков on 28.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Bananza1_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bananza1.1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bananza1.1
+//
+//  Created by Артём Коротков on 28.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
