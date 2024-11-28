0646d3033dde8f9b7979ef296a22d0963c970d7d
keshew <artyom.korotckov2017@yandex.ru>
2024-11-28 19:06:15 +0500

    fix

--- Bananza1.1/Bananza1_1App.swift
+++ Bananza1.1/Bananza1_1App.swift
@@ -1,17 +1,13 @@
-//
-//  Bananza1_1App.swift
-//  Bananza1.1
-//
-//  Created by Артём Коротков on 28.11.2024.
-//
-
 import SwiftUI
 
 @main
 struct Bananza1_1App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MenuView()
+                .onAppear {
+                    UserDefaultsManager().firstLaunch()
+                }
         }
     }
 }
